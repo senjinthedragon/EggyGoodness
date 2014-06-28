6a541a04e219819eb2a01e95ae0823319bf6ad70
senjinthedragon <senjin@hatchling.org>
2014-06-28 13:14:38 +0200

    1.7.10 Commit

--- src/main/java/org/hatchling/eggygoodness/reference/Reference.java
+++ src/main/java/org/hatchling/eggygoodness/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "EggyGoodness";
     public static final String MOD_NAME = "Eggy Goodness";
-    public static final String VERSION = "1.7.2-1.0";
+    public static final String VERSION = "1.7.10-1.0";
     public static final String SERVER_PROXY_CLASS = "org.hatchling.eggygoodness.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "org.hatchling.eggygoodness.proxy.ClientProxy";
 }
