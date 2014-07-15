3cdb0e61e0ec928c851c501eadab8785be30c9da
senjinthedragon <senjin@hatchling.org>
2014-07-15 19:05:52 +0200

    SMP working and tested.

--- src/main/java/org/hatchling/eggygoodness/eventhook/EventHookContainer.java
+++ src/main/java/org/hatchling/eggygoodness/eventhook/EventHookContainer.java
@@ -1,6 +1,8 @@
 package org.hatchling.eggygoodness.eventhook;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -16,7 +18,7 @@ public class EventHookContainer
 
         if(event.action == PlayerInteractEvent.Action.LEFT_CLICK_BLOCK)
         {
-            if(player.getCurrentEquippedItem().getItem() == Items.egg || player.getCurrentEquippedItem().getItem() == ModItems.unpeeledSoftBoiledEgg || player.getCurrentEquippedItem().getItem() == ModItems.unpeeledHardBoiledEgg)
+            if(player.getCurrentEquippedItem() != null && (player.getCurrentEquippedItem().getItem() == Items.egg || player.getCurrentEquippedItem().getItem() == ModItems.unpeeledSoftBoiledEgg || player.getCurrentEquippedItem().getItem() == ModItems.unpeeledHardBoiledEgg))
             {
                 ItemStack returnStack = null;
                 String soundFX = "";
